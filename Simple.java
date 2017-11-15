85ec81f6c04db61cbb6f23865c3acf8febac69c5
Moriah Sherman <moriahsherman@gmail.com>
2017-11-15 12:34:15 +0000

    Adding first comment.

--- Simple.java
+++ Simple.java
@@ -1,3 +1,4 @@
+// This is a simple java program to be used as a base example.
 class Simple{
 	public static void main(String args[]){
 		System.out.println("This is another simple Java program.");
